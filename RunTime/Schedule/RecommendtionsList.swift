15ca8e6274383b9a9a6587935f5fb289953816e9
Sarah Perez <perezsarah14@gmail.com>
2024-04-06 19:07:28 -0400

    reverted back to schedule run functionality

--- RunTime/Schedule/RecommendtionsList.swift
+++ RunTime/Schedule/RecommendtionsList.swift
@@ -33,21 +33,19 @@ struct RecommendtionsList: View {
         from the list.
     */
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [Color(.sRGB, red: 0.4, green: 0.9, blue: 1.0), Color.purple]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.all)
-            .overlay(
             VStack {
                 if storeManager.events.isEmpty {
                     MessageView(message: .events)
                 } else {
                     List(selection: $selection) {
-                        var recommendations = createRecommendations(calendarEvents: storeManager.events)
+                        let recommendations = createRecommendations(calendarEvents: storeManager.events)
                         ForEach(recommendations, id: \.self) { event in
                             VStack(alignment: .leading, spacing: 7) {
                                 Image(systemName: "figure.run")
                                     .resizable()
                                     .scaledToFit()
-                                    .frame(height: 30)
+                                    .frame(height: 50)
+                                Text(event.title).font(.title)
                                 HStack {
                                     Text(event.startDate, style: .date)
                                         .foregroundStyle(.primary)
@@ -62,22 +60,18 @@ struct RecommendtionsList: View {
                             } .swipeActions {
                                 Button("Schedule") {
                                     scheduleRunAction(event: event)
-                                }.sheet(isPresented: $showEventEditViewController,
-                                        onDismiss: didDismissEventEditController, content: {
-                                    EventEditViewController(event: $selectedRunTime, eventStore: store)
-                             })
+                                }
                                 .tint(.green)
                             }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding().background(Color.clear).cornerRadius(15).shadow(color: Color.black.opacity(0.2), radius: 5)
+                            .sheet(isPresented: $showEventEditViewController,
+                                   onDismiss: didDismissEventEditController, content: {
+                               EventEditViewController(event: $selectedRunTime, eventStore: store)
+                        })
                         }
                     }
                     .environment(\.editMode, $editMode)
-                    .scrollContentBackground(.hidden)
                 }
             }
-                .cornerRadius(10)
-        )
             .alertErrorMessage(message: alertMessage, title: alertTitle, isPresented: $shouldPresentError)
     }
     
@@ -86,38 +80,37 @@ struct RecommendtionsList: View {
     }
     
     func createRecommendations(calendarEvents: [EKEvent]) -> [EKEvent] {
+        var recommendations: [EKEvent] = []
+        let runIntensities = getRunIntensity()
+        let runCount = 1...6
         
+        for i in runCount {
+            var event = EKEvent(eventStore: store)
+            var intensity = runIntensities[Int.random(in: 1..<6)]
+            // get random run intensity
+            
+            event.title = intensity.title
+            event.startDate = Date()
+            event.endDate = Date().addingTimeInterval(TimeInterval(intensity.duration))
+            
+            recommendations.append(event)
+        }
         
-//        ForEach(storeManager.events, id: \.self) { event in
-//
-//
-//        }
-        
-        return storeManager.events
+        return recommendations
+    }
+    
+    private func getRunIntensity() -> [RunIntensity] {
+        return [RunIntensity.easy, RunIntensity.moderate, RunIntensity.tempo, RunIntensity.threshold, RunIntensity.long, RunIntensity.race]
     }
     
     func scheduleRunAction(event: EKEvent) {
-//        Task {
-//            if #unavailable(iOS 17) {
-//                do {
-//                    guard try await store.requestAccess(to: .event) else {
-//                        selection = nil
-//                        let message = "The app doesn't have permission to access calendar data. Please grant the app access to Calendar in Settings."
-//                        showError(message, title: "Calendar access denied")
-//                        return
-//                    }
-//                } catch {
-//                    selection = nil
-//                    showError(error.localizedDescription, title: "Failed to request calendar access")
-//                    return
-//                }
-//            }
             
             selectedRunTime = event
             
-            showEventEditViewController.toggle()
+            self.showEventEditViewController = true;
             print(showEventEditViewController)
-            print(selectedRunTime!)
+            
+        
 //        }
     }
     
