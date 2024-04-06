e18f79717bf05469f6e45f607c07e4e29c5188e1
Sarah Perez <perezsarah14@gmail.com>
2024-04-06 19:41:58 -0400

    minuet ui changes

---
+++ RunTime/WelcomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeScreenView.swift
+//  RunTime
+//
+//  Created by Sarah Perez on 4/6/24.
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeScreenView()
+}
